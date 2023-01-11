2c9c9a3556cfbc73095b464204c9c6c4449ec9e6
Victor <Novakoski2021@gmail.com>
2023-01-11 13:51:54 -0300

    fixed: add basic function updated comment

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import "express-async-errors";
 import handlerError from "./errors/handlerError";
+import commentsRoutes from "./routes/comments.routes";
 import recipesRouter from "./routes/recipes.routes";
 
 const app = express();
 
+app.use(express.json())
 /** routes here */
 
 app.use("/recipes", recipesRouter);
+app.use("", commentsRoutes);
 
 app.use(handlerError);
 
--- src/routes/recipes.routes.ts
+++ src/routes/recipes.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createRecipeController } from "../controllers/recipes/createRecipe.controller";
+import { updatedCommentController } from "../controllers/comments.controller";
 
 const recipesRouter = Router();
 
 recipesRouter.post("", createRecipeController);
+recipesRouter.patch("/:recipe_id/comments/:comment_id", updatedCommentController);
 
 export default recipesRouter;
