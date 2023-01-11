f92705692d11f70032e5a436cd126e8583b8c405
Mateo <mateogpaz@gmail.com>
2023-01-11 11:55:05 -0300

    feat/CreateUserRoute

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import handlerError from "./errors/handlerError";
-import userRoutes from "./routes/user.routes";
 import "express-async-errors";
 
 const app = express();
 app.use(express.json());
-
-app.use("/users", userRoutes);
 
 /** routes here */
 
