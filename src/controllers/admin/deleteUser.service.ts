3a92cb580dc399e05c041ebd7b36ec0ac19ec8b4
Felipe Bulhões <felipebbulhoes@gmail.com>
2023-01-12 10:24:13 -0300

    feat: adm func delete user

---
+++ src/controllers/admin/deleteUser.service.ts
@@ -0,0 +1,8 @@
+import { Request ,Response } from "express";
+import { deleteUserService } from "../../services/admin/deleteUser.controller";
+
+export const deleteUserController = async (req: Request, res: Response) => {
+    const userId: string = req.body.userId
+    const deleteUser: object = await deleteUserService(userId)
+    return res.json(deleteUser)
+}
