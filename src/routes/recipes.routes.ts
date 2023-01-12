cd7ff70ca01679b745839994cd668eac4d73d354
CarlosX26 <cjunior1@live.com>
2023-01-12 10:10:12 -0300

    feat: add service get recipe by id

--- src/routes/recipes.routes.ts
+++ src/routes/recipes.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { updatedCommentController } from "../controllers/comments/createComment.controller";
 import { createRecipeController } from "../controllers/recipes/createRecipe.controller";
 import { listAllRecipesController } from "../controllers/recipes/listAllRecipes.controller";
+import { listRecipeController } from "../controllers/recipes/listRecipe.controller";
 import { ensureAuthMiddleware } from "../middlewares/ensureAuthMiddleware.middleware";
 import { verifySchemaMiddleware } from "../middlewares/verifySchema.middleware";
 import { recipesSchema } from "../schemas/recipes/recipesSchema";
@@ -9,6 +10,7 @@ import { recipesSchema } from "../schemas/recipes/recipesSchema";
 const recipesRouter = Router();
 
 recipesRouter.get("", listAllRecipesController);
+recipesRouter.get("/:id", listRecipeController);
 recipesRouter.post(
     "",
     ensureAuthMiddleware,
