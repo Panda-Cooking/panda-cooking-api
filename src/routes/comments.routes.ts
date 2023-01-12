33cdea1e9a01e2d84138481eadbc9048198036bf
Eduardo Perondi de Almeida <dudupalmeida@gmail.com>
2023-01-12 10:57:21 -0300

    feat: added token verification when creating a comment

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import createCommentController from "../controllers/comments/createComment.controller";
 import deleteCommentController from "../controllers/comments/deleteComment.controller";
 import { listCommentsController } from "../controllers/comments/listComments.controller";
+import { ensureAuthMiddleware } from "../middlewares/ensureAuthMiddleware.middleware";
 
 const commentsRoutes = Router();
 
-commentsRoutes.post("", createCommentController);
+commentsRoutes.post("", ensureAuthMiddleware, createCommentController);
 commentsRoutes.get("", listCommentsController);
 commentsRoutes.delete("/:id", deleteCommentController);
 
