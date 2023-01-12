0ea88b459fbcf07382950142511030d8da413273
CarlosX26 <cjunior1@live.com>
2023-01-12 10:38:39 -0300

    fix: user needs token for delete recipe

--- src/controllers/recipes/deleteRecipe.controller.ts
+++ src/controllers/recipes/deleteRecipe.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import deleteRecipeService from "../../services/recipes/deleteRecipe.service";
 
 export const deleteRecipeController = async (req: Request, res: Response) => {
-    const data = await deleteRecipeService(req.params.id);
+    const data = await deleteRecipeService(req.userId, req.params.id);
 
     return res.status(204).json(data);
 };
--- src/services/recipes/deleteRecipe.service.ts
+++ src/services/recipes/deleteRecipe.service.ts
@@ -2,15 +2,29 @@ import AppDataSource from "../../data-source";
 import { Recipe } from "../../entities/recipes.entity";
 import AppError from "../../errors/appError";
 
-const deleteRecipeService = async (recipeId: string): Promise<{}> => {
+const deleteRecipeService = async (
+    userId: string,
+    recipeId: string
+): Promise<{}> => {
     const recipeRepo = AppDataSource.getRepository(Recipe);
 
-    const findRecipe = await recipeRepo.findOneBy({
-        id: recipeId,
+    const findRecipe = await recipeRepo.findOne({
+        where: {
+            id: recipeId,
+            user: {
+                id: userId,
+            },
+        },
+        relations: {
+            user: true,
+        },
     });
 
     if (!findRecipe) {
-        throw new AppError("Recipe not found", 404);
+        throw new AppError(
+            "Recipe not found / User can only delete their own recipes",
+            404
+        );
     }
 
     await recipeRepo.remove(findRecipe);
