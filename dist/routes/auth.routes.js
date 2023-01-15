8fa7ba745e1faa0b832ec138c895f473f4045169
Carlos Junio <106787456+CarlosX26@users.noreply.github.com>
2023-01-15 15:54:18 -0300

    delete /dist from main

--- dist/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth/auth.controller");
-const verifySchema_middleware_1 = require("../middlewares/verifySchema.middleware");
-const authSchema_1 = require("../schemas/auth/authSchema");
-const authRouter = (0, express_1.Router)();
-authRouter.post("", (0, verifySchema_middleware_1.verifySchemaMiddleware)(authSchema_1.authSchemaRequest), auth_controller_1.authController);
-exports.default = authRouter;
